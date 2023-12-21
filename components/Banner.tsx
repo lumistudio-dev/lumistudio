1e913dce251a148483f8276f2689bd8731e9c511
HindiaTimur <contact.mochammadbagas@gmail.com>
2023-12-21 08:02:50 +0700

    Mengganti Navbar 2.0 dan Warna Banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,13 +3,13 @@ import { Button } from './ui/button';
 
 function Banner() {
   return (
-    <div className='bg-pattern bg-[#49B1E4]'>
-      <div className='container w-full h-full bg-gradient-to-t from-[#49B1E4] pt-44 pb-60'>
-        <div className='flex flex-col items-center justify-center text-white'>
-          <div className='font-bold text-5xl lg:text-8xl text-center uppercase drop-shadow-md'>
+    <div className='bg-pattern bg-[#0CAAE3]'>
+      <div className='container w-full h-full bg-gradient-to-t from-[#0CAAE3] to-[#0CAAE3]/10 pt-60 pb-60'>
+        <div className='flex flex-col items-center justify-center'>
+          <div className='font-bold text-5xl lg:text-8xl text-center uppercase drop-shadow-md text-white'>
             Lumi Studio
           </div>
-          <div className='font-normal text-xl lg:text-2xl text-center my-5'>
+          <div className='font-normal text-xl lg:text-2xl text-center my-5 text-white/75'>
             Jangkau pelanggan baru di Instagram bersama Kami!
           </div>
           <Button
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,60 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from './ui/sheet';
+import { Button } from './ui/button';
+import { AlignRight, Figma, Github, Instagram } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 function Navbar() {
   return (
-    <nav className='container absolute top-0 left-0 right-0 flex items-center justify-between py-5'>
-      <div className='w-full md:w-4/12 lg:w-5/12 flex justify-center md:justify-start'>
+    <nav className='container absolute top-0 left-0 right-0 flex items-center justify-center py-5'>
+      <div className='hidden md:block md:w-5/12 lg:w-4/12 justify-start pr-6'>
+        <div className='flex justify-between uppercase font-medium text-sm text-white/80 tracking-widest'>
+          <Link href='/'>Layanan Kami</Link>
+          <Link href='/'>Alur Kerja</Link>
+        </div>
+      </div>
+      <div className='w-full md:w-2/12 lg:w-4/12 flex md:justify-center'>
         <Link href='/'>
           <Image
             src='/assets/logo/logo.png'
             alt='logo'
-            width={80}
-            height={80}
+            width={1000}
+            height={1000}
+            className='w-16 h-16 md:w-24 md:h-24 lg:w-32 lg:h-32'
           />
         </Link>
       </div>
-      <div className='hidden md:block md:w-8/12 lg:w-7/12 justify-end'>
+      <div className='hidden md:block md:w-5/12 lg:w-4/12 justify-end pl-6'>
         <div className='flex justify-between uppercase font-medium text-sm text-white/80 tracking-widest'>
-          <Link href='/'>Layanan Kami</Link>
-          <Link href='/'>Alur Kerja</Link>
           <Link href='/'>Tentang Kami</Link>
           <Link href='/'>Hubungi Kami</Link>
+        </div>
+      </div>
+      <div className='flex gap-2'>
+        <div className='md:hidden'>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant='secondary' size='icon'>
+                <AlignRight />
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <div className='flex flex-col text-white gap-10 py-10'>
+                <Link href='/'>Layanan Kami</Link>
+                <Link href='/'>Alur Kerja</Link>
+                <Link href='/'>Tentang Kami</Link>
+                <Link href='/'>Hubungi Kami</Link>
+              </div>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
     </nav>
