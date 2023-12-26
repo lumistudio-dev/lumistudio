bdaf3e561afb8e1dcbf7189f1607d1e71373c769
HindiaTimur <contact.mochammadbagas@gmail.com>
2023-12-26 12:08:42 +0700

    Menambahkan Portofolio Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from '@/components/Banner';
 import Campaign from '@/components/Campaign';
 import OurServices from '@/components/OurServices';
+import Portfolio from '@/components/portfolio/Portfolio';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Banner />
       <OurServices />
       <Campaign />
+      <Portfolio />
     </div>
   );
 }
