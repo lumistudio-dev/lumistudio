e8183a4f7bb3202ea7b4d914d85c7dfbcb555ee7
HindiaTimur <contact.mochammadbagas@gmail.com>
2023-12-29 04:17:41 +0700

    Memperbaiki Link dan Image

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -6,6 +6,7 @@ import {
   IconBrandInstagram,
   IconMail,
 } from '@tabler/icons-react';
+import Link from 'next/link';
 
 function About() {
   return (
@@ -26,11 +27,11 @@ function About() {
           </Button>
         </div>
       </div>
-      <div className='flex flex-col-reverse md:flex-row'>
-        <div className='w-full md:w-1/2 md:mx-10'>
+      <div className='flex flex-col-reverse md:flex-row items-center'>
+        <div className='w-full md:w-1/2 px-0 md:pr-5 lg:px-10'>
           <div className='text-white mb-5'>
             <div className='text-2xl font-bold mb-4'>Who we are?</div>
-            <div className='text-justify'>
+            <div className='text-justify text-white'>
               <span className='font-bold'>
                 LUMI adalah Social Media Agency{' '}
               </span>
@@ -43,22 +44,28 @@ function About() {
           <div className='text-white'>
             <div className='text-2xl font-bold mb-4'>Connect with Us:</div>
             <div className='flex flex-col gap-2'>
-              <div className='flex gap-2'>
+              <Link href='mailto:lumistudio06@gmail.com' className='flex gap-2'>
                 <IconMail />
                 lumistudio06@gmail.com
-              </div>
-              <div className='flex gap-2'>
+              </Link>
+              <Link
+                href='https://wa.me/message/LULXNY427CQWI1'
+                className='flex gap-2'
+              >
                 <IconBrandWhatsapp />
                 (+62) 851 2345 6789
-              </div>
-              <div className='flex gap-2'>
-                <IconBrandInstagram /> @lumistudio
-              </div>
+              </Link>
+              <Link
+                href='https://www.instagram.com/lumi.std/'
+                className='flex gap-2'
+              >
+                <IconBrandInstagram /> @lumi.std
+              </Link>
             </div>
           </div>
         </div>
-        <div className='w-full md:w-1/2 md:mx-10 relative mb-10 md:mb-0'>
-          <div className='absolute -z-10 -top-12 -left-16'>
+        <div className='w-full md:w-1/2 px-0 md:pl-5 lg:mx-10 relative mb-10 md:mb-0'>
+          <div className='absolute -z-10 -top-7 md:-top-6 -left-7 md:-left-2 lg:-top-12 lg:-left-16'>
             <Image
               src='/assets/frame.svg'
               alt='logo'
@@ -72,7 +79,7 @@ function About() {
             alt='logo'
             width={1000}
             height={1000}
-            className='rounded-tr-[60px] rounded-bl-[60px] md:rounded-tr-[80px] md:rounded-bl-[80px] lg:rounded-tr-[120px] lg:rounded-bl-[120px]'
+            className='drop-shadow-2xl rounded-tr-[60px] rounded-bl-[60px] md:rounded-tr-[80px] md:rounded-bl-[80px] lg:rounded-tr-[120px] lg:rounded-bl-[120px]'
           />
         </div>
       </div>
