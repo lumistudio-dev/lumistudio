9d88de16782f7d979de4036995fcf4f3b79b55ef
HindiaTimur <contact.mochammadbagas@gmail.com>
2023-12-29 03:26:46 +0700

    Menambahkan Workflow Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from '@/components/Banner';
 import OurServices from '@/components/OurServices';
+import Workflow from '@/components/workflow/Workflow';
 
 export default function Home() {
   return (
     <div>
       <Banner />
       <OurServices />
+      <Workflow />
     </div>
   );
 }
